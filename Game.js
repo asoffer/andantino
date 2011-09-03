b484e8900c21dc71323e025773a54acb18eb023a
Andy <soffer801@gmail.com>
2011-09-03 14:14:34 -0700

    small simplification to game

--- Game.js
+++ Game.js
@@ -9,29 +9,33 @@ function Game(){
     this.ctx = this.canvas.getContext("2d");
 
 
+    //list of all built hexes
     this.hexes = new List();
+    
+    this.iter = new ListIterator(this.hexes,function(h){h.draw(document.getElementById("canvas").getContext("2d"),20);});
 }
 
 Game.prototype.extend({
     init: function(){
-	h1 = new Hex(0,0);
-	h1.color = "blue";
-	this.hexes.pushBack(h1);
+	h = new Hex(0,0);
+	h.color = "blue";
+	this.hexes.pushBack(h);
+	h = new Hex(0,1);
+	h.color = "red";
+	this.hexes.pushBack(h);
+	h = new Hex(1,1);
+	h.color = "gray";
+	this.hexes.pushBack(h);
+	h = new Hex(1,2);
+	h.color = "green";
+	this.hexes.pushBack(h);
+	h = new Hex(4,2);
+	h.color = "green";
+	this.hexes.pushBack(h);
 
-	h2 = new Hex(0,1);
-	h2.color = "red";
-	this.hexes.pushBack(h2);
-
-	h3 = new Hex(1,1);
-	h3.color = "gray";
-	this.hexes.pushBack(h3);
     },
 
     draw: function(){
-	var iter = new ListIterator(this.hexes,function(h){h.draw(document.getElementById("canvas").getContext("2d"),20);});
-	iter.apply();
-//	this.h1.draw(this.ctx,20);
-//	this.h2.draw(this.ctx,20);
-//	this.h3.draw(this.ctx,20);
+	this.iter.apply();
     }
 });
